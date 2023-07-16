92b9dfb0db7b4b9d2ca4f8726654f50f1b5415e7
seung_ho_choi.s <chltmdgh517@naver.com>
2023-07-16 18:26:34 +0900

    .

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{flex:2, backgroundColor: "tomato" }}></View>
+      <View style={{flex:1, backgroundColor: "teal" }}></View>
+      <View style={{flex:1, backgroundColor: "blue"}}></View>
+      <StatusBar style="light"/>
     </View>
   );
 }
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize:28,
+    color:"red",
+  }
 });
